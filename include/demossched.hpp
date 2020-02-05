15839c7deb19dcf8a2784535c4c54353081989bd
Michal Sojka <michal.sojka@cvut.cz>
2020-02-06 00:41:24 +0100

    Simplify CHECK macro

--- include/demossched.hpp
+++ include/demossched.hpp
@@ -5,7 +5,7 @@
 #include <chrono>
 #include <err.h>
 
-#define CHECK(expr) ({ decltype (expr) ret = (expr); if (ret == -1) throw std::system_error(errno, std::generic_category(), std::string(__PRETTY_FUNCTION__) + ": " #expr); ret; })
+#define CHECK(expr) ({ auto ret = (expr); if (ret == -1) throw std::system_error(errno, std::generic_category(), std::string(__PRETTY_FUNCTION__) + ": " #expr); ret; })
 
 class DemosSched
 {
