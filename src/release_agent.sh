8e04fb7aabce71fbba723744e79d0cf63f9bd5c6
Jiri Zahora <jiri.zahora@cvut.cz>
2020-02-13 23:12:27 +0100

    Create correct release agent to clean up cgroups

--- src/release_agent.sh
+++ src/release_agent.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-rmdir `ls -d /sys/fs/cgroup/freezer/my_cgroup/*/*/`
-rmdir `ls -d /sys/fs/cgroup/freezer/my_cgroup/*/`
-rmdir `ls -d /sys/fs/cgroup/cpuset/my_cgroup/*/*/`
-rmdir `ls -d /sys/fs/cgroup/cpuset/my_cgroup/*/`
-rmdir `ls -d /sys/fs/cgroup/unified/user.slice/user-1000.slice/user@1000.service/my_cgroup/*/*/`
-rmdir `ls -d /sys/fs/cgroup/unified/user.slice/user-1000.slice/user@1000.service/my_cgroup/*/`
+FREEZER="/sys/fs/cgroup/freezer/my_cgroup"
+CPUSET="/sys/fs/cgroup/cpuset/my_cgroup"
+UNIFIED="/sys/fs/cgroup/unified/user.slice/user-1000.slice/user@1000.service/my_cgroup"
+
+# kill all procs in freezer
+procs_files=($(find "$FREEZER" -name *.procs))
+
+for i in "${procs_files[@]}"
+do
+    while read line
+    do
+        kill $line
+    done < "$i"
+done
+
+# unfreeze all freezers
+freezers=($(find "$FREEZER" -name *freezer.state))
+
+for i in "${freezers[@]}"
+do
+    #echo "$i"
+    echo "THAWED" > "$i"
+done
+
+# remove all cgroups
+d="/*"
+rmdir `ls -d $FREEZER$d$d/ 2>/dev/null ` 2>/dev/null
+rmdir `ls -d $FREEZER$d/ 2>/dev/null ` 2>/dev/null
+rmdir `ls -d $CPUSET$d$d/ 2>/dev/null ` 2>/dev/null
+rmdir `ls -d $CPUSET$d/ 2>/dev/null ` 2>/dev/null
+rmdir `ls -d $UNIFIED$d$d/ 2>/dev/null ` 2>/dev/null
+rmdir `ls -d $UNIFIED$d/ 2>/dev/null ` 2>/dev/null
