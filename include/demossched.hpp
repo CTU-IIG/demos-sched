e75ded2fe3f1f3b51cccdc2a0aeb68e7f250fee3
Jiri Zahora <jiri.zahora@cvut.cz>
2020-02-14 13:48:26 +0100

    Remove DemosSched class

--- include/demossched.hpp
+++ include/demossched.hpp
@@ -2,27 +2,10 @@
 #define DEMOSSCHED_HPP
 
 #include <iostream>
-#include <chrono>
 #include <err.h>
 
 #define CHECK(expr) ({ auto ret = (expr); if (ret == -1) throw std::system_error(errno, std::generic_category(), std::string(__PRETTY_FUNCTION__) + ": " #expr); ret; })
 
 #define VERBOSE
-
-class DemosSched
-{
-public:
-    static void init();
-    static void set_cgroup_paths(std::string freezer, std::string cpuset);
-
-protected:
-    static std::string freezer_path;
-    static std::string cpuset_path;
-    static std::string unified_path;
-
-    static std::chrono::steady_clock::time_point start_time;
-
-    static bool initialized;
-};
 
 #endif // DEMOSSCHED_HPP
