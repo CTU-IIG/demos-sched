d69678a8c4e7b8e90963267df4097b084faa4977
Michal Sojka <michal.sojka@cvut.cz>
2020-02-25 09:55:10 +0100

    Add acutest test framework and a test for timerfd

--- src/timerfd_test.cpp
+++ src/timerfd_test.cpp
@@ -1,42 +1,26 @@
+#include "acutest.h"
+
 #include <iostream>
 #include "timerfd.hpp"
 
 using namespace std;
 using namespace ev;
 
-void tim_cb(timer &t, int revents)
-{
-    cout << "ev::timer" << endl;
-}
-
-void io_cb(ev::io &t, int revents)
-{
-    cout << "ev::io" << endl;
-}
-
-int main(int argc, char *argv[])
+static void test()
 {
     default_loop loop;
     timerfd tfd(loop);
-    timer tim(loop);
-    ev::io i(loop);
 
-    i.set<io_cb>();
-    i.start(0, ev::READ);
-
-    tfd.set([](){cout << "timerfd expired" << endl;});
-    tfd.start(chrono::steady_clock::now() + 1s);
-
-    tim.set<tim_cb>();
-    tim.start(0.1);
-
-    loop.run(ev::ONCE);
-
-    tfd.stop();
-    i.stop();
-    tim.start(2);
+    bool expired = false;
+    tfd.set([&](){cout << "timerfd expired" << endl; expired = true; });
+    tfd.start(chrono::steady_clock::now() + 0s);
 
     loop.run();
 
-    return 0;
+    TEST_CHECK(expired);
 }
+
+TEST_LIST = {
+    { "timerfd", test },
+    { 0 }
+};
