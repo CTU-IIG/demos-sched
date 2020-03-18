d2ea9f683526bab31f31e9cf40b06715b0283751
Jiri Zahora <jiri.zahora@cvut.cz>
2020-03-18 18:15:50 +0100

    Add api function description

--- include/demos-sch.h
+++ include/demos-sch.h
@@ -1,7 +1,19 @@
 #ifndef DEMOSSCH_H
 #define DEMOSSCH_H
 
+/*
+ * Initialize interface for demos-sched.
+ * @return -1 if initialization failed, otherwise 0
+ */
 int demos_init();
+
+/*
+ * Signalizes to the scheduler that all work for the current
+ * window is completed (see spec.)
+ * Block until the new scheduling by demos-sched.
+ * @return 0
+ * @todo make sure, that process cannot be scheduld multiple times in one window
+ */
 int demos_completed();
 
 #endif // DEMOSSCH_H
