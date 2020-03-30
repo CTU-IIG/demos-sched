94b8ba00da9db495d071aa54ad3ad6184a9b6806
Michal Sojka <michal.sojka@cvut.cz>
2020-03-30 17:01:10 +0200

    Add simple API test

--- src/api_test.c
+++ src/api_test.c
@@ -4,15 +4,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    int i = 0;
+
     if (demos_init() == -1)
         err(1, "demos_init");
 
-    while (true) {
-        printf("new_period\n");
-        sleep(1);
-        printf("completed\n");
+    while (argc == 1 || ++i < argc) {
+        if (argc == 1) {
+            printf("new_period\n");
+            sleep(1);
+            printf("completed\n");
+        } else {
+            printf("%s\n", argv[i]);
+        }
+
         if (demos_completed() == -1)
             err(1, "demos_completed");
     }
--- test/0100-api.t
+++ test/0100-api.t
@@ -1,6 +1,15 @@
 #!/bin/bash
 . testlib
-plan_tests 1
+plan_tests 3
 
 api-test
 is $? 1 "Running outside of demos-sched fails"
+
+# Check that demos schedules first two iterations of the sub-process
+readarray -t lines < <(demos-sched -C "{
+    windows: [ {length: 50, slices: [ { cpu: 0, sc_partition: SC1 }] } ],
+    partitions: [ { name: SC1, processes: [ { cmd: api-test first second, budget: 500 } ] } ]
+}")
+echo PATH=$PATH >&2
+is "${lines[0]}" "first" "first iteration"
+is "${lines[1]}" "second" "second iteration"
