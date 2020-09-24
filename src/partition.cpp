61ba8e2c9db3307e26de1a1b3c2d64984bb4bc43
Michal Sojka <michal.sojka@cvut.cz>
2020-09-24 23:34:37 +0200

    Don't call process groups "Xtodo", but "procX"
    ... where X is a number.

--- src/partition.cpp
+++ src/partition.cpp
@@ -57,7 +57,7 @@ void Partition::add_process(ev::loop_ref loop,
     // cerr<< cmd_name <<endl;
 
     processes.emplace_back(
-      loop, to_string(proc_count) + "todo", *this, argv, budget, budget_jitter);
+      loop, "proc" + to_string(proc_count), *this, argv, budget, budget_jitter);
     proc_count++;
     current = --processes.end();
     current->exec();
