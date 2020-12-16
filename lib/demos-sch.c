8d319012d5abfe1339534d09d5c1db1ddcfd2d7e
Matej Kafka <git@a-n.cc>
2020-12-16 23:11:53 +0100

    Added optional logging to process library
    Verbose logging can be enabled by setting the DEMOS_LIB_VERBOSE preprocessor variable while building. Also added `lib_verbose` Meson option that sets this variable when `true`.

--- lib/demos-sch.c
+++ lib/demos-sch.c
@@ -5,26 +5,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#ifdef DEMOS_LIB_VERBOSE
+#define LOG(...)                                                                                   \
+    do {                                                                                           \
+        fprintf(stderr, "[DEMOS] ");                                                               \
+        fprintf(stderr, __VA_ARGS__);                                                              \
+        fprintf(stderr, "\n");                                                                     \
+    } while (0)
+#else
+#define LOG(...)                                                                                   \
+    do {                                                                                           \
+    } while (0)
+#endif
+
 static int fd_completed, fd_new_period;
 
 int demos_init()
 {
     char *str = getenv("DEMOS_FDS");
     if (!str) {
+        LOG("Error: Environment variable DEMOS_FDS is missing");
         errno = ENOKEY;
         return -1;
     }
 
     if (sscanf(str, "%d,%d", &fd_completed, &fd_new_period) != 2) {
+        LOG("Error: Failed to load configuration from DEMOS_FDS environment variable");
         errno = EBADMSG;
         return -1;
     }
 
+    LOG("Process library set up.");
     return 0;
 }
 
 int demos_completed()
 {
+    LOG("Notifying scheduler of completion and suspending process...");
+
     uint64_t buf = 1;
     // notify demos
     if (write(fd_completed, &buf, sizeof(buf)) == -1)
@@ -34,5 +52,6 @@ int demos_completed()
     if (read(fd_new_period, &buf, sizeof(buf)) == -1)
         return -1;
 
+    LOG("Process resumed");
     return 0;
 }
