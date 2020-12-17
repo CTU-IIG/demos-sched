3cdce52c169184afef09f8e38d61da8e16380a0a
Matej Kafka <git@a-n.cc>
2020-12-17 01:43:14 +0100

    Updated docstrings for demos-sch.h (no functional changes)

--- lib/demos-sch.h
+++ lib/demos-sch.h
@@ -1,18 +1,23 @@
 #ifndef DEMOSSCH_H
 #define DEMOSSCH_H
 
-/*
+/**
  * Initialize interface for demos-sched.
- * @return Zero if successful, -1 otherwise and errno is set appropriately
+ *
+ * Will be called automatically by `demos_completed()` on first
+ * invocation if not called explicitly before.
+ *
+ * @return 0 if successful, -1 otherwise and errno is set appropriately
  */
 int demos_init();
 
-/*
- * Signalizes to the scheduler that all work for the current
- * window is completed (see spec.)
- * Block until the new scheduling by demos-sched.
- * @return Zero if successful, -1 otherwise and errno is set appropriately.
- * @todo make sure, that process cannot be scheduld multiple times in one window
+/**
+ * Signalizes to the scheduler that all work for the current window is completed (see spec.)
+ * Blocks until the process is scheduled again by demos-sched.
+ *
+ * @return 0 if successful, -1 otherwise and errno is set appropriately.
+ *
+ * @todo make sure, that process cannot be scheduled multiple times in one window
  * @todo set errno
  */
 int demos_completed();
