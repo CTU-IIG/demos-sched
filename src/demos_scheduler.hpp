32cd35652790b4b2b951539da79e488b81adcd91
Matej Kafka <git@a-n.cc>
2021-03-02 00:10:12 +0100

    ! Added CPU frequency scaling code in PowerManager
    I'm not sure this is ready for PR now - I tested it on i.MX8, but not much on Intel CPUs, which have the added complexity of a custom `intel_pstate` driver with different behavior from the generic governors. I'd appreciate more testing before PR.
    
    Also, there is the question of correctly solving the governor cleanup on crash, which should probably be solved together with cgroup cleanup and other related resources in a unified way (probably using systemd or a separate launcher).

--- src/demos_scheduler.hpp
+++ src/demos_scheduler.hpp
@@ -1,6 +1,9 @@
+#pragma once
+
 #include "log.hpp"
 #include "majorframe.hpp"
 #include "partition_manager.hpp"
+#include "power_manager.hpp"
 #include "slice.hpp"
 #include <chrono>
 #include <ev++.h>
@@ -12,6 +15,7 @@ public:
         : loop(loop)
         , mf(mf)
         , partition_manager(partitions)
+        , power_manager()
     {
         // setup completion callback
         partition_manager.set_completion_cb(std::bind(&DemosScheduler::completion_cb, this));
@@ -52,6 +56,7 @@ private:
     ev::loop_ref loop;
     MajorFrame &mf;
     PartitionManager partition_manager;
+    PowerManager power_manager;
     ev::sig sigint{ loop };
     ev::sig sigterm{ loop };
 
