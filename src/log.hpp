25b4a7915c040ce2a31a9ade2b1fe537ec3056c7
Matej Kafka <git@a-n.cc>
2021-03-01 04:10:27 +0100

    Silence clang warnings about diagnostic pragmas for spdlog
    Clang does not know the `-Wsuggest-attribute=const` warning ignored here, and there doesn't seem to be a flag that would work for both clang and gcc. Simplest solution seems to be to suppress the warning about unknown warning. :)
    
    However, GCC doesn't know -Wunknown-warning-option, so it complains. So we'll also suppress that and now it seems to work correctly and silently on both GCC and Clang.

--- src/log.hpp
+++ src/log.hpp
@@ -1,9 +1,13 @@
-#ifndef LOG_HPP
-#define LOG_HPP
+#pragma once
 
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Weffc++"
+// this suppresses the GCC warning about unknown pragma below
+#pragma GCC diagnostic ignored "-Wpragmas"
+// this suppresses the warning about unknown warning in Clang below, but it's not known for GCC
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
+// this pragma is supported in GCC, but not Clang
 #pragma GCC diagnostic ignored "-Wsuggest-attribute=const"
+#pragma GCC diagnostic ignored "-Weffc++"
 #include "spdlog/spdlog.h"
 #pragma GCC diagnostic pop
 
@@ -17,5 +21,3 @@ extern std::shared_ptr<spdlog::logger> logger;
  * @param force_colors - if true, spdlog always uses color escape codes in logs
  */
 void initialize_logger(std::string pattern, bool load_env_levels, bool force_colors);
-
-#endif
