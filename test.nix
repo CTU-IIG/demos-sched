e83ecf5128076aa314a1532b7d2d5095ffd5e53c
Michal Sojka <michal.sojka@cvut.cz>
2021-03-08 18:23:22 +0100

    nix: Drop tests with gcc7
    Newly added std::filestystem dependency is not available in gcc
    version 7 (and earlier). Drop it from tests and keep only test with
    gcc8 and later.
    
    This requires a bit of hackery around tests with meson-0.49.2. For
    this old meson version, nixpkgs hardcode the compiler to be the
    default one from nixpkgs and does not support changing it via stdenv.
    The default compiler in that old nixpkgs version happens to be gcc7.
    The applied workaround replaces meson command with a shell function,
    which unsets the variables with hardcoded compiler so that meson uses
    the default gcc and g++ from stdenv.

--- test.nix
+++ test.nix
@@ -6,19 +6,28 @@ let
     url = "https://github.com/nixos/nixpkgs/archive/4599f2bb9a5a6b1482e72521ead95cb24e0aa819.tar.gz";
     sha256 = "04xr4xzcj64d5mf4jxzn5fsbz74rmf90qddp3jcdwj4skyik946d";
   }) { };
-  demosSchedWithCC = cc:
+  demosSchedWithCC = { pkgs, cc, overrideMesonCC ? false }:
     let
-      pkgs = pkgsUnstable;
       myStdenv = with pkgs; (overrideCC stdenv (builtins.getAttr cc pkgs));
     in with pkgs;
-    callPackage ./demos-sched.nix {
+    callPackage ./demos-sched.nix ({
       stdenv = myStdenv;
       meson = meson.overrideAttrs (attrs: { depsHostHostPropagated = [ ]; });
       libyamlcpp = libyamlcpp.override { stdenv = myStdenv; };
-    };
+    } // pkgs.lib.optionalAttrs overrideMesonCC {
+      extraAttrs = {
+        # Meson in old nixpkgs hardcodes CC and CXX and does not obey stdenv - override it here
+        preConfigure = ''
+          meson() {
+            unset CC CXX
+            command meson "$@"
+          }
+        '';
+      };
+    });
 in {
-  meson-0-49-2 = with pkgsMeson-0-49-2; callPackage ./demos-sched.nix { };
   unstable = with pkgsUnstable; callPackage ./demos-sched.nix { };
-  gcc8 = demosSchedWithCC "gcc8";
-  gcc7 = demosSchedWithCC "gcc7";
+  meson-0-49-2 = demosSchedWithCC { pkgs = pkgsMeson-0-49-2; cc = "gcc8"; overrideMesonCC = true; };
+  gcc8 = demosSchedWithCC { pkgs = pkgsUnstable; cc= "gcc8"; };
+  gcc9 = demosSchedWithCC { pkgs = pkgsUnstable; cc= "gcc9"; };
 }
