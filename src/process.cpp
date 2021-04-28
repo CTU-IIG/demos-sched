3471f3a0fcb8d1ed05015296ef1c05412c1bd85e
Matej Kafka <git@a-n.cc>
2021-04-29 00:46:21 +0200

    Process budget is now `budget +- jitter/2` instead of `budget +- jitter`

--- src/process.cpp
+++ src/process.cpp
@@ -24,8 +24,8 @@ Process::Process(ev::loop_ref loop,
                  milliseconds budget,
                  milliseconds budget_jitter,
                  bool has_initialization)
-    // `budget +- jitter` seems more intuitive than `budget +- (jitter / 2)
-    : jitter_distribution_ms(-budget_jitter.count(), budget_jitter.count())
+    // budget +- (jitter / 2)
+    : jitter_distribution_ms(-budget_jitter.count() / 2, budget_jitter.count() - budget_jitter.count() / 2)
     , loop(loop)
     , efd_continue(CHECK(eventfd(0, EFD_SEMAPHORE)))
     , part(partition)
